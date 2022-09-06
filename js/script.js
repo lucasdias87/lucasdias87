f12642704815aa503a58faf4858f701fd8e8762c
Lucas Dias <lucas.dias1987lu@gmail.com>
2022-09-06 18:46:11 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -19,15 +19,24 @@ function darkm(){
    
     
     document.querySelector("body").style.backgroundColor="#20232a";
-    document.querySelector("h2").style.color="";
-    document.querySelector("h3").style.color="";
+   
     document.querySelector("h4").style.color="";
+    document.querySelector("h2").style.color="";  
+    document.querySelector("h1").style.color="";
+   
+    
     localStorage.clear("purple", "true");
 
 
 }
 function lyth(){
-    document.querySelector("body").style.backgroundColor="purple";
+
+    document.querySelector("body").style.backgroundColor="#c2c3c7";
+
+   
+    document.querySelector("h4").style.color="#171219";
+    document.querySelector("h2").style.color="#171219";  
+    document.querySelector("h1").style.color="#2f4143";
     
     localStorage.setItem("purple", "true");
    
@@ -40,3 +49,4 @@ function lyth(){
 
 
 }
+
