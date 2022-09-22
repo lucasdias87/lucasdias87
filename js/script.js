01f8a621bb8bb75217e227a5e7a8acb2a6343b40
Lucas Dias <lucas.dias1987lu@gmail.com>
2022-09-21 22:24:32 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,52 +1,13 @@
 
 
 
-const loop = setInterval(() => {
-    
-    if(localStorage.getItem("purple")=="true"){
-        lyth();
-       
-    }
-   
-   
-
-}   , 1000);
-
-
-
-
-function darkm(){
-   
-    
-    document.querySelector("body").style.backgroundColor="#20232a";
-   
-    document.querySelector("h4").style.color="";
-    document.querySelector("h2").style.color="";  
-    document.querySelector("h1").style.color="";
-   
-    
-    localStorage.clear("purple", "true");
-
-
+function clicMenu(){
+if(itens.style.display =='block'){
+    itens.style.display ='none';
+  
 }
-function lyth(){
-
-    document.querySelector("body").style.backgroundColor="#c2c3c7";
-
-   
-    document.querySelector("h4").style.color="#171219";
-    document.querySelector("h2").style.color="#171219";  
-    document.querySelector("h1").style.color="#2f4143";
-    
-    localStorage.setItem("purple", "true");
-   
-   
-
-
-
-   
-
-
-
+else
+{
+    itens.style.display ='block';
 }
-
+}
